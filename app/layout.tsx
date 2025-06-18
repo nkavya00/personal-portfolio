327149fc7b6c03dfd121627e41c2a8f595af9dd8
nkavya00 <nkavya@umich.edu>
2025-06-17 22:22:00 -0700

    adding favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,18 @@ export const metadata: Metadata = {
   title: "Kavya Narayan",
   description: "Kavya Narayan's Personal Website",
   generator: "v0.dev",
+  icons: {
+    apple: [
+      { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" }
+    ],
+    icon: [
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" }
+    ],
+    other: [
+      { url: "/site.webmanifest", rel: "manifest" }
+    ]
+  }
 }
 
 export default function RootLayout({
